6f51b7133a1927f3f633398be7bc6b235bcf1d09
BanisBT <banisbt@gmail.com>
2021-12-13 17:33:16 +0200

    hbs Mustache

--- src/main/kotlin/com/example/plugins/Templating.kt
+++ src/main/kotlin/com/example/plugins/Templating.kt
@@ -1,11 +1,9 @@
 package com.example.plugins
 
 import com.github.mustachejava.DefaultMustacheFactory
-import io.ktor.mustache.Mustache
-import io.ktor.mustache.MustacheContent
 import io.ktor.application.*
+import io.ktor.mustache.*
 import io.ktor.response.*
-import io.ktor.request.*
 import io.ktor.routing.*
 
 fun Application.configureTemplating() {
@@ -15,8 +13,23 @@ fun Application.configureTemplating() {
 
     routing {
         get("/html-mustache") {
-            call.respond(MustacheContent("index.hbs", mapOf("user" to MustacheUser(1, "user1"))))
+            call.respond(
+                MustacheContent(
+                    "index.hbs", mapOf(
+                        "user" to MustacheUser(1, "user1"),
+                        "listas" to list,
+                        "mapArray" to mapArray
+                    )
+                )
+            )
         }
     }
 }
+
 data class MustacheUser(val id: Int, val name: String)
+
+val list = listOf<String>("Java", "Kotlin", "JS")
+
+val mapArray = mapOf(
+    "tittle" to listOf<String>("1", "2", "3")
+)
